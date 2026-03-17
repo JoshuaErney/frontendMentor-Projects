2e7f9228fcf83aa7ed9c057e20d0b2bca9fad9b3
Joshua Erney <joshuaerney@icloud.com>
2026-03-17 18:04:35 -0400

    got my json file connected to my html using the fetech API and promises.

--- productListWithCart-main/js/main.js
+++ productListWithCart-main/js/main.js
@@ -3,10 +3,56 @@ const confirmOrderDialog = document.querySelector('#shoppingCart-Dialog');
 
 const startNewOrder = document.querySelector('button#new-order');
 
+const cardTemplate = document.querySelector('#card-template');
+const menuContainer = document.querySelector('#menuItem-container');
+
 confirmOrderBtn.addEventListener('click', (e) => {
     confirmOrderDialog.showModal();
-})
+});
 
 startNewOrder.addEventListener('click', () => {
     confirmOrderDialog.close();
-})
+});
+
+async function getJSONData() {
+    const url = "./data/desserts.json";
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        result.forEach(item => {
+            let card = cardTemplate.content.cloneNode(true);
+            const cardTitle = card.querySelector('h3.food-name');
+            const cardCategory = card.querySelector('data.food-category');
+            const cardType = card.querySelector('data.food-type');
+            const cardPrice = card.querySelector('data.food-price');
+            const cardqty = card.querySelector('data.food-quantity');
+
+            cardTitle.textContent = item.name
+
+            cardCategory.textContent = item.category
+            cardCategory.value = item.category
+
+            cardType.textContent = item.type
+            cardType.value = item.type
+
+            cardPrice.textContent = item.price
+            cardPrice.value = item.price
+
+            cardqty.textContent = item.quantity
+            cardqty.value = item.quantity
+
+            menuContainer.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+getJSONData();
